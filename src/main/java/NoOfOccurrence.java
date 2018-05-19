ebfc8427e87fb563b164e4bd66a8e14a4ef89e6d
Sophia <Sophia@Dafnee>
2018-05-19 17:56:20 +0530

    No of occurrence from Antony

---
+++ src/main/java/NoOfOccurrence.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+
+public class NoOfOccurrence {
+
+	public static void main(String[] args) {
+		//String txt1 = "testleaf";
+		System.out.println("Enter a String and a character");
+		Scanner scan = new Scanner(System.in);
+		String input1 = scan.nextLine();
+		
+		//System.out.println("Enter any character : ");
+		String input2 = scan.nextLine();
+		char inputCharacter = input2.charAt(0);
+		
+		char[] charArray = input1.toCharArray();
+		
+		int occurrence = 0;
+		for (char c : charArray) {
+			if(c == inputCharacter)
+				occurrence+=1;
+		}
+		System.out.println("No of occurrence of "+inputCharacter+" is "+occurrence);
+		scan.close();
+	}
+
+}
